c33cea12150b766ff53e9b9554ba0f5bfbdb9d87
Heitor Meira <htrmeira@gmail.com>
2016-03-08 15:41:27 -0300

    initializing repository with some configurations and scripts

---
+++ monitors/config-monitors.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+XRANDR=/usr/bin/xrandr
+
+# display status for all video ports in this machine.
+# We are excluding the original and primary monitor (LVDS1)
+# And VIRTUAL1 to avoid mistakes
+list_connected_mointors() {
+	$XRANDR | grep -v ^' \|^VIRTUAL1\|^LVDS1' | grep -iw 'connected' | awk '{ print $1 }'
+}
+
+# Counts that number of connected monitors
+num_extra_monitors() {
+	list_connected_mointors | wc -w
+}
+
+# This method basically alternates between on and two monitors.
+# The secondary monitor will be configured left of the primary,
+# the primary monitor is LVDS1.
+get_connected_monitors_names() {
+	local n_extra_monitors=$(num_extra_monitors)
+
+	if [ $n_extra_monitors == 0 ]; then
+		echo "Configuring to primary monitor only..."
+		$XRANDR --auto
+	elif [ $n_extra_monitors == 1 ]; then
+		echo "Secondary found..."
+		local monitor=$(list_connected_mointors);
+
+		echo "Configuring $monitor..."
+		$XRANDR --output $monitor --auto --right-of LVDS1
+	else
+		echo "Unknown number of monitors"
+		echo "This might mean more than 2 monitors or an internal error"
+		exit 15;
+	fi
+
+}
+
+get_connected_monitors_names;
---
+++ old-scripts/double-monitors-hdmi-aoc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xrandr --output HDMI-1 --auto --right-of LVDS-1 --mode 1280x800
